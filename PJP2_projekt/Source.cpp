8c7a6b77d147855954b56c17daa23d89f96ca557
BobekRW <robertwyszecki@gmail.com>
2016-01-20 23:02:42 +0100

    The Last

--- PJP2_projekt/Source.cpp
+++ PJP2_projekt/Source.cpp
@@ -13,6 +13,13 @@
 
 using namespace std;
 
+bool collision(float x, float y, float ex, float ey, int height, int width){
+	if (x + width<ex || x>width + ex || y + height<ey || y>height + ey){
+		return false;
+	}
+
+	return true;
+}
 
 int main(){
 
@@ -32,7 +39,6 @@ int main(){
 	bool instruction = false;
 	bool game = false;
 	bool jump = false;
-	bool collision = false;
 	bool motion = false;
 	int choiceY = 330;
 	int pejzazX = 0;
@@ -41,17 +47,23 @@ int main(){
 	int playerY = 480;
 	int moveSpeed = 5;
 	int score = 0;
+	int highscore = 0;
 	const int gravity = 1;
 	int jumpSpeed = 12;
 	int velx,vely;
 	float position = 0;
-	int obstacle1X = 790;
+	int obstacle1X = 800;
+	int obstacle1Y = 505;
+	int obstacle2X = 1200;
+	int obstacle2Y = 505;
+	int obstacle3X = 1500;
+	int obstacle3Y = 505;
 	velx = vely = 0;
 	position = pejzazX;
-	const int maxFrame = 2;
+	const int maxFrame = 4;
 	int curFrame = 0;
 	int frameCount = 0;
-	int frameDelay = 8;
+	int frameDelay = 7;
 
 	if (!display){
 		al_show_native_message_box(NULL, "Error", "Display", "Can not initialize display", NULL, NULL);
@@ -72,9 +84,14 @@ int main(){
 	ALLEGRO_BITMAP *pejzaz = al_load_bitmap("Pejzaz.png");
 	ALLEGRO_BITMAP *pejzaz2 = al_load_bitmap("Pejzaz2.png");
 	ALLEGRO_BITMAP *przeszkoda = al_load_bitmap("przeszkoda.png");
+	ALLEGRO_BITMAP *przekszoda2 = al_load_bitmap("przeszkoda.png");
+	ALLEGRO_BITMAP *przeszkoda3 = al_load_bitmap("przeszkoda.png");
+	ALLEGRO_BITMAP *skok=al_load_bitmap("PostacSkok.png");
 	ALLEGRO_BITMAP *animation[maxFrame];
 	animation[0] = al_load_bitmap("Postac.png");
 	animation[1] = al_load_bitmap("Postac2.png");
+	animation[2] = al_load_bitmap("Postac3.png");
+	animation[3] = al_load_bitmap("Postac4.png");
 	ALLEGRO_TIMER *timer = al_create_timer(1.0 / FPS);
 	ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
 
@@ -135,8 +152,9 @@ int main(){
 			al_get_keyboard_state(&keyState);
 			if (al_key_down(&keyState, ALLEGRO_KEY_UP) && jump){
 				vely = -jumpSpeed;
-				playerY = 480;
+				playerY = 481;
 				jump = false;
+				motion = true;
 			}
 			else if (al_key_down(&keyState, ALLEGRO_KEY_ENTER)){
 				if (choiceY == 440){
@@ -183,72 +201,123 @@ int main(){
 			al_clear_to_color(al_map_rgb(0, 0, 0));
 		}
 
-		if (collision){
-			al_draw_textf(font, al_map_rgb(255, 255, 255), ScreenHeigth/1.5, ScreenWeidth / 2.5, ALLEGRO_ALIGN_CENTRE, "GAME OVER");
-			al_flip_display();
-			al_clear_to_color(al_map_rgb(0, 0, 0));
-			al_rest(1.0);
-			score = 0;
-			position = 0;
-			moveSpeed = 5;
-			playerX = 10;
-			playerY = 480;
-			pejzazX = 0;
-			pejzaz2X = pejzazX + 800;
-			obstacle1X = 790;
-			game = false;
-			collision = false;
-			starter = true;
-		}
-
 		if (game){
 
 			pejzazX -= moveSpeed;
 			pejzaz2X -= moveSpeed;
 			obstacle1X -= moveSpeed;
+			obstacle2X -= moveSpeed;
+			obstacle3X -= moveSpeed;
 
 			if (pejzazX <= -800){
 				pejzazX = 0;
 				pejzaz2X = pejzazX + 800;
 			}
 
-			if (obstacle1X <= 0){
-				obstacle1X = 790;
+			if (obstacle1X <= -700){
+				obstacle1X = 800;
+				obstacle2X = 1200;
+				obstacle3X = 1500;
 			}
 
-			if (playerX == obstacle1X && playerY==480){
-				al_rest(1.0);
-				jump = false;
-				collision = true;
-			}
 
 			if (score <= 30){
 				moveSpeed = 5;
 			}
 			else if(score >30  && score<=80){
-				moveSpeed = 7;
+				moveSpeed = 8;
 			}
 			else if(score>150 && score<=300){
-				moveSpeed = 9;
+				moveSpeed = 10;
 			}
 			else{
-				moveSpeed = 11;
+				moveSpeed = 12;
+			}
+
+			if (collision(playerX, playerY, obstacle1X, obstacle1Y, 32, 32)){
+				al_rest(0.5);
+				al_draw_textf(font, al_map_rgb(255, 255, 255), ScreenHeigth / 1.5, ScreenWeidth / 2.5, ALLEGRO_ALIGN_CENTRE, "GAME OVER");
+				al_flip_display();
+				al_clear_to_color(al_map_rgb(0, 0, 0));
+				al_rest(1.0);
+				score = 0;
+				position = 0;
+				moveSpeed = 5;
+				playerX = 10;
+				playerY = 481;
+				pejzazX = 0;
+				pejzaz2X = pejzazX + 800;
+				obstacle1X = 800;
+				obstacle2X = 1200;
+				obstacle3X = 1500;
+				game = false;
+				starter = true;
+			}
+
+			if (collision(playerX, playerY, obstacle2X, obstacle2Y, 32, 32)){
+				al_rest(0.5);
+				al_draw_textf(font, al_map_rgb(255, 255, 255), ScreenHeigth / 1.5, ScreenWeidth / 2.5, ALLEGRO_ALIGN_CENTRE, "GAME OVER");
+				al_flip_display();
+				al_clear_to_color(al_map_rgb(0, 0, 0));
+				al_rest(1.0);
+				score = 0;
+				position = 0;
+				moveSpeed = 5;
+				playerX = 10;
+				playerY = 481;
+				pejzazX = 0;
+				pejzaz2X = pejzazX + 800;
+				obstacle1X = 800;
+				obstacle2X = 1200;
+				obstacle3X = 1500;
+				game = false;
+				starter = true;
+			}
+
+			if (collision(playerX, playerY, obstacle3X, obstacle3Y, 32, 32)){
+				al_rest(0.5);
+				al_draw_textf(font, al_map_rgb(255, 255, 255), ScreenHeigth / 1.5, ScreenWeidth / 2.5, ALLEGRO_ALIGN_CENTRE, "GAME OVER");
+				al_flip_display();
+				al_clear_to_color(al_map_rgb(0, 0, 0));
+				al_rest(1.0);
+				score = 0;
+				position = 0;
+				moveSpeed = 5;
+				playerX = 10;
+				playerY = 481;
+				pejzazX = 0;
+				pejzaz2X = pejzazX + 800;
+				obstacle1X = 800;
+				obstacle2X = 1200;
+				obstacle3X = 1500;
+				game = false;
+				starter = true;
 			}
 
 			position += moveSpeed;
 			score = (int) (position + moveSpeed)/100;
 
+			if (highscore < score)
+				highscore = score;
+
 			al_draw_bitmap(pejzaz, pejzazX, 0, NULL);
 			al_draw_bitmap(pejzaz2, pejzaz2X, 0, NULL);
 			al_draw_textf(font, al_map_rgb(255, 255, 255), ScreenHeigth, ScreenWeidth/50, ALLEGRO_ALIGN_LEFT, "SCORE:%d", score);
+			al_draw_textf(font, al_map_rgb(255, 255, 255), ScreenHeigth/2.5, ScreenWeidth / 50, ALLEGRO_ALIGN_RIGHT, "HIGHSCORE:%d", highscore);
 			if (++frameCount >= frameDelay){
 				if (++curFrame >= maxFrame)
 					curFrame = 0;
 
 				frameCount = 0;
 			}
-			al_draw_bitmap(animation[curFrame], 10, playerY, 0);
-			al_draw_bitmap(przeszkoda, obstacle1X, 505, 0);
+
+			if (motion && jump)
+				al_draw_bitmap(animation[curFrame], playerX, playerY, NULL);
+			if (!jump)
+				al_draw_bitmap(skok, playerX, playerY, NULL);
+			al_draw_bitmap(przeszkoda, obstacle1X, obstacle1Y, 0);
+			al_draw_bitmap(przekszoda2, obstacle2X, obstacle2Y, 0);
+			al_draw_bitmap(przeszkoda3, obstacle3X, obstacle3Y, 0);
 			al_flip_display();
 			al_clear_to_color(al_map_rgb(0, 0, 0));
 
@@ -264,6 +333,9 @@ int main(){
 	al_destroy_bitmap(pejzaz);
 	al_destroy_bitmap(pejzaz2);
 	al_destroy_bitmap(przeszkoda);
+	al_destroy_bitmap(przekszoda2);
+	al_destroy_bitmap(przeszkoda3);
+	al_destroy_bitmap(skok);
 	al_destroy_timer(timer);
 	al_destroy_event_queue(event_queue);
 	return 0;
